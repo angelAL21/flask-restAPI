aea1852a7b05f69f57fd00851fb6ad797791283c
unknown <angell_2104@hotmail.com>
2022-02-23 12:54:49 -0600

    added some comments

--- test.py
+++ test.py
@@ -2,9 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10, "name": "angel", "views":1000})
-print(response.json())
-input()
 
-response = requests.get(BASE + "video/1")
-print(response.json())
+# data=[{"name": "video_name", "views": 100000, "likes": 10000}]
+
+# for i in range(len(data)):
+#     response = requests.put(BASE + "video/7" + str(i), data[i])
+#     print(response.json())
+    
+
+response = requests.get(BASE + "video/2") #get one video with id.
+#response = requests.patch(BASE + "video/2"), {"views":999999, "likes": 99999} patch one video with id.
+
+# input()
+# response = requests.get(BASE + "video/6")
+print(response.json)

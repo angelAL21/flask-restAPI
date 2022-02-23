9e7dae0b55b2088fc5db6cbe7e46904de33932ce
unknown <angell_2104@hotmail.com>
2022-02-23 10:44:39 -0600

    added the dict name and returning the name of that dict

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from flask_restful import Api, Resource
 app= Flask(__name__)
 api = Api(app)
 
+names= {"tim":{"age":19, "gender": "male"}, "bill":{"age": 20, "gender": "male"}}
+
 #helloworld class test.
 class HelloWorld(Resource):
-    def get(self):
-        return {"data":"hello world"}
+    def get(self, name):
+        return names[name]
 
 #endpoint helloworld.
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == '__main__':
     app.run(debug=True)

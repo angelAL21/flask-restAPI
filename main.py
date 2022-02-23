9debfbed93aa80658c0cc14bbb2f2e9080a03a19
unknown <angell_2104@hotmail.com>
2022-02-23 11:04:21 -0600

    Video class added and get and put def too. we called a video_put_args to pass args like name, views and likes of a video

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app= Flask(__name__)
 api = Api(app)
 
-names= {"tim":{"age":19, "gender": "male"}, "bill":{"age": 20, "gender": "male"}}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="name of video is required", required=True)
+video_put_args.add_argument("views", type=int, help="views of video", required=True)
+video_put_args.add_argument("likes", type=int, help="likes on video", required=True)
 
-#helloworld class test.
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+videos={}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+    
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] =args
+        return videos[video_id], 201
 
 #endpoint helloworld.
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == '__main__':
     app.run(debug=True)

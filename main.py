909eaa77509b1a32e0da0cd638c095d2675cb1d8
unknown <angell_2104@hotmail.com>
2022-02-23 10:30:21 -0600

    endpoint helloworld added

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ from flask_restful import Api, Resource
 app= Flask(__name__)
 api = Api(app)
 
+#helloworld class test.
+class HelloWorld(Resource):
+    def get(self):
+        return {"data":"hello world"}
+
+#endpoint helloworld.
+api.add_resource(HelloWorld, "/helloworld")
+
 if __name__ == '__main__':
     app.run(debug=True)

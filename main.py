225d2eaf298cfc34b81600c6fa9053271dc39f7c
unknown <angell_2104@hotmail.com>
2022-02-23 12:25:06 -0600

    fixed the get and put funcs because we now use db, we create db with id, name, views and likes. Also manage the error if a video does not exists or the id was already taken

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app= Flask(__name__)
 api = Api(app)
+app.config["SQLALCHEMY_DATABASE_URI"]= 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views =db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+    
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})"
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="name of video is required", required=True)
 video_put_args.add_argument("views", type=int, help="views of video", required=True)
 video_put_args.add_argument("likes", type=int, help="likes on video", required=True)
 
-videos={}
-
-def abort_ifVideo_NotFound(video_id):
-    if video_id not in videos:
-        abort(404, message="video id is not valid or does not exist")
-        
-def abort_ifVideo_Exists(video_id):
-    if video_id in videos:
-        abort(409, message="video already exists")
-
+resource_fields={
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer,
+}
 class Video(Resource):
+    @marshal_with(resource_fields) #decorator to marshal resoruce_Fields
+    #get videos by id.
     def get(self, video_id):
-        abort_ifVideo_NotFound(video_id)
-        return videos[video_id]
+        result= VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video not found")
+        print(result)
     
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        args = video_put_args.parse_args()
-        videos[video_id] =args
-        return videos[video_id], 201
-    
-    def delete(self, video_id):
-        abort_ifVideo_Exists(video_id)
-        del videos[video_id]
-        return '', 204
+        args = video_put_args.parse_args() #parse and get all the resources from db.
+        result= VideoModel.query.filter_by(id= video_id).first()
+        if result:
+            abort(409, message="video id taken")
+        video= VideoModel(id= video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
 
 #endpoint helloworld.
 api.add_resource(Video, "/video/<int:video_id>")
